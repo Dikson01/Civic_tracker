c9ed697dbae0fce82c3d9fe50a0db36429bdaba1
Dikson01 <147234213+Dikson01@users.noreply.github.com>
2025-08-02 18:16:27 +0530

    Delete Civic track/civictrack directory

--- Civic track/civictrack/tracker/serializers.py
+++
@@ -1,8 +0,0 @@
-# issues/serializers.py
-from rest_framework import serializers
-from .models import CivicIssue
-
-class CivicIssueSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CivicIssue
-        fields = '__all__'
